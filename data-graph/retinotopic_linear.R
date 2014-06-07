d1b03d984b13aab0565f9d41024e6765816ed743
Myke Walton <walton.myke@gmail.com>
2014-06-06 19:22:35 -0700

    R^2

--- data-graph/retinotopic_linear.R
+++ data-graph/retinotopic_linear.R
@@ -31,6 +31,27 @@ data.summary <- summarySEwithin(data=alldata, measurevar="wRT", withinvars=c("TR
 l = dim(data.summary)[1]
 c = rep(410,l)
 data.summary$wRT = c - data.summary$wRT
+
+########## compute R2s
+golomb_S = c(8, 21, 21)
+golomb_I = c(8, 5, 13)
+golomb_R = c(24, 15, 9.5)
+
+epic_S = c(9.804074, 46.113835, 59.938499)
+epic_I = c(33.635838, 31.693449, 378.0808)
+epic_R = c(78.352804, 59.209275, 43.710729)
+
+R_S = cor(golomb_S,epic_S)
+R_I = cor(golomb_I,epic_I)
+R_R = cor(golomb_R,epic_R)
+
+Rsq_S = R_S*R_S
+Rsq_I = R_I*R_I
+Rsq_R = R_R*R_R
+
+rlbl_S <- paste("R^2 == ", round(Rsq_S,4))
+rlbl_I <- paste("R^2 == ", round(Rsq_I,4))
+rlbl_R <- paste("R^2 == ", round(Rsq_R,4))
 ########################################### CREATE GRAPH
 
 #some settings to make a pretty graph (not really required)
@@ -48,14 +69,10 @@ ggplot(data.summary, aes(ymax = (wRT + se), ymin = (wRT - se),group=TRIAL_TYPE, 
   geom_errorbar(aes(ymax = (wRT + se), ymin = (wRT - se)), width=0.25, position=position_dodge(.1)) +
   geom_line(position=position_dodge(.1)) +
   geom_point(position=position_dodge(.1)) +
+  annotate("text", x = 3, y = 80, label = rlbl_S, parse=TRUE, colour="blue") +
+  annotate("text", x = 3, y = 77, label = rlbl_I, parse=TRUE, colour="darkred") +
+  annotate("text", x = 3, y = 74, label = rlbl_R, parse=TRUE, colour="darkgreen") +
   my.theme + labs(y=paste('RT ',modifier,'Difference (ms)',sep=""), x='Probe Delay (MS after saccade completion)') + 
   ggtitle(paste("  Attentional Facilitation by Probe Location (N=",N,")",sep="")) +
   theme(legend.position = "right")
-
-
-
-
-
-
-
 
